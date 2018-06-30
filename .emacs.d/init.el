72802da592e68885da3dedead4b61f44f54279c4
keima12 <keima1224@gmail.com>
2018-07-01 01:56:49 +0900

    空フォルダを追加した。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,13 @@
 ;;---------------------------------------------
 ;; load-pathを定義する関数を定義し、ロードパスの追加する
 ;;---------------------------------------------
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
